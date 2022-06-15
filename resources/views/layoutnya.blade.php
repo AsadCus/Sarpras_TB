76adbf4f4f51619583fa571653b480cb6c4552db
zeinmukaffi <zeinau.16@gmail.com>
2022-06-15 11:52:49 +0700

    tambahin excel ya angga

--- resources/views/layoutnya.blade.php
+++ resources/views/layoutnya.blade.php
@@ -68,8 +68,8 @@
               <li class="nav-item {{ 'barang' == request()->path() ? 'active' : '' }}">
                 <a href="/barang" class="nav-link"><i class="fas fa-th"></i><span>Barang</span></a>
               </li>
-              <li class="nav-item {{ 'operator' == request()->path() ? 'active' : '' }}">
-                <a href="{{ url('operator') }}" class="nav-link"><i class="fas fa-users"></i> <span>Operator</span></a>      
+              {{-- <li class="nav-item {{ 'operator' == request()->path() ? 'active' : '' }}">
+                <a href="{{ url('operator') }}" class="nav-link"><i class="fas fa-users"></i> <span>Operator</span></a>       --}}
               <li class="nav-item {{ 'inventory' == request()->path() ? 'active' : '' }}">
                 <a href="/inventory" class="nav-link"><i class="fas fa-archive"></i> <span>Inventory</span></a>
               </li>
