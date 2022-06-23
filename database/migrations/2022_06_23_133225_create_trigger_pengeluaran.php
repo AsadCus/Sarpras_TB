57cfd7faad1d0482623d6a029adfe5edca76495d
HafidBuroiroh <hafidburoiroh09@gmail.com>
2022-06-23 13:38:02 +0700

    trigger pengeluaran

---
+++ database/migrations/2022_06_23_133225_create_trigger_pengeluaran.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('CREATE TRIGGER after_pengeluaran after INSERT ON barang_keluars
+            FOR EACH ROW
+            BEGIN UPDATE inventori set
+            stock = stock - NEW.jumlah_keluar
+            WHERE barang_id = NEW.barang_id;
+            END'
+        );
+
+        DB::unprepared('CREATE TRIGGER after_pengeluaran_2 after INSERT ON barang_keluars
+        FOR EACH ROW
+        BEGIN UPDATE inventori set
+        jumlah_tersedia = jumlah_tersedia - NEW.jumlah_keluar
+        WHERE barang_id = NEW.barang_id;
+        END'
+    );
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP TRIGGER after_pengeluaran');
+        DB::unprepared('DROP TRIGGER after_pengeluaran_2');
+    }
+};
