62501f8901bcbbdfee637cf73dd2c51c696f8045
HafidBuroiroh <hafidburoiroh09@gmail.com>
2022-06-23 15:05:45 +0700

    trigger done

--- database/migrations/2022_06_23_133225_create_trigger_pengeluaran.php
+++ database/migrations/2022_06_23_133225_create_trigger_pengeluaran.php
@@ -30,6 +30,37 @@ return new class extends Migration
         END'
     );
 
+ DB::unprepared('CREATE TRIGGER before_update_pengeluaran_stock before UPDATE ON barang_keluars
+        FOR EACH ROW
+        BEGIN UPDATE inventori set
+        stock = stock + OLD.jumlah_keluar
+        WHERE barang_id = OLD.barang_id;
+        END'
+    );
+    DB::unprepared('CREATE TRIGGER after_update_pengeluaran_stock after UPDATE ON barang_keluars
+            FOR EACH ROW
+            BEGIN UPDATE inventori set
+            stock = stock - NEW.jumlah_keluar
+            WHERE barang_id = NEW.barang_id;
+            END'
+        );
+
+        DB::unprepared('CREATE TRIGGER before_update_pengeluaran before UPDATE ON barang_keluars
+        FOR EACH ROW
+        BEGIN UPDATE inventori set
+        jumlah_tersedia = jumlah_tersedia + OLD.jumlah_keluar
+        WHERE barang_id = OLD.barang_id;
+        END'
+    );
+
+
+        DB::unprepared('CREATE TRIGGER after_update_pengeluaran after UPDATE ON barang_keluars
+        FOR EACH ROW
+        BEGIN UPDATE inventori set
+        jumlah_tersedia = jumlah_tersedia - NEW.jumlah_keluar
+        WHERE barang_id = NEW.barang_id;
+        END'
+    );
 
     }
 
@@ -42,5 +73,9 @@ return new class extends Migration
     {
         DB::unprepared('DROP TRIGGER after_pengeluaran');
         DB::unprepared('DROP TRIGGER after_pengeluaran_2');
+        DB::unprepared('DROP TRIGGER before_update_pengeluaran_stock');
+        DB::unprepared('DROP TRIGGER after_update_pengeluaran_stock');
+        DB::unprepared('DROP TRIGGER before_update_pengeluaran');
+        DB::unprepared('DROP TRIGGER after_update_pengeluaran');
     }
 };
